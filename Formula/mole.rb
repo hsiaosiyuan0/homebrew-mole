1122572868d4634349a4be6d830d98e0522930c0
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2021-11-25 16:26:19 +0800

    feat: v0.0.3

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -1,8 +1,8 @@
 class Mole < Formula
   desc "Tools to deeply process the frontend projects"
   homepage "https://github.com/hsiaosiyuan0/mole"
-  url "https://github.com/hsiaosiyuan0/mole/releases/download/v0.0.2/mole_v0.0.2_darwin_amd64.tgz"
-  sha256 "d31b7c176866a0dae4e79c582297b251b4205db8ca8ca9a653e4ae6354a8dff2"
+  url "https://github.com/hsiaosiyuan0/mole/releases/download/v0.0.3/mole_v0.0.3_darwin_amd64.tgz"
+  sha256 "553fa68d8d35152be392e10f4e11f17b733c990fb05ba7d815286429c18c73a6"
   version "0.0.3"
 
   def install
