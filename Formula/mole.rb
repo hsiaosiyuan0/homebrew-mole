aa0ca05bba71f04fdf22427a2fe1b782b3eed271
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2022-06-29 12:36:03 +0800

    feat: new ver

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -1,9 +1,15 @@
 class Mole < Formula
   desc "Tools to deeply process the frontend projects"
   homepage "https://github.com/hsiaosiyuan0/mole"
-  url "https://github.com/hsiaosiyuan0/mole/releases/download/v0.0.3/mole_v0.0.3_darwin_amd64.tgz"
-  sha256 "553fa68d8d35152be392e10f4e11f17b733c990fb05ba7d815286429c18c73a6"
-  version "0.0.3"
+  version "0.0.1"
+
+  if Hardware::CPU.arm?
+    url 'https://github.com/hsiaosiyuan0/mole/releases/download/0.0.1/mole-0.0.1-darwin-arm64.tar.gz'
+    sha256 '442201a6e7b7686194cf853bd7fbbd54aafd4ce0fa0c7ce0ad341de42ff9b209'
+  else
+    url 'https://github.com/hsiaosiyuan0/mole/releases/download/0.0.1/mole-0.0.1-darwin-amd64.tar.gz'
+    sha256 '3b16c4cbd4f99f3fda399707a6f457673d1144477ddac9cc0294d014ed529514'
+  end
 
   def install
     bin.install "mole"
